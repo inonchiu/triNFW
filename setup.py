a7b606b581fc8ddb08c52dca71421ef7d29c0380
inonchiu <inonchiu@usm.lmu.de>
2016-08-09 15:14:47 +0800

    add setup.py
    also change the structure of directory

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='triNFW',
+    version='1.0',
+    packages=['triNFW'],
+      package_data={
+      },
+    url='',
+    license='',
+    author='I-Non Chiu',
+    author_email='inchiu@asiaa.sinica.edu.tw',
+    description='Trixial NFW Model'
+)
